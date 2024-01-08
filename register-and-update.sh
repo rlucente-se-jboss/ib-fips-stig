9a8a59b8edd7fd49a672176322fc2ddae1708064
rlucente-se-jboss <rlucente@redhat.com>
2024-01-08 10:41:25 -0500

    register with RHSM

---
+++ register-and-update.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+. $(dirname $0)/demo.conf
+
+[[ $EUID -ne 0 ]] && exit_on_error "Must run as root"
+
+subscription-manager register \
+    --username $USERNAME --password $PASSWORD || exit 1
+subscription-manager role --set="Red Hat Enterprise Linux Server"
+subscription-manager service-level --set="Self-Support"
+subscription-manager usage --set="Development/Test"
+subscription-manager attach
+
+dnf -y update
+dnf -y clean all
+
