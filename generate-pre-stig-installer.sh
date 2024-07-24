2f8b91d0224f951a9b8337f26d92b16b054621e9
rlucente-se-jboss <rlucente@redhat.com>
2024-07-24 15:08:06 -0400

    updates to RHEL 9.4

---
+++ generate-pre-stig-installer.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+. $(dirname $0)/demo.conf
+
+[[ $EUID -eq 0 ]] && exit_on_error "Must NOT run as root"
+
+# build the Pre-STIG-ISO blueprint file
+cat > pre-stig-installer.toml <<EOF
+name = "Pre-STIG-ISO"
+description = "Simplified Installer ISO for the Pre STIG rpm-ostree image"
+version = "0.0.1"
+
+[customizations]
+fips = true
+
+[[customizations.user]]
+name = "${EDGE_USER}"
+description = "Admin User"
+password = "${EDGE_PASS_HASH}"
+groups = ["wheel"]
+EOF
+
+# add needed packages and append the remote for rpm-ostree updates for
+# the edge device
+cat >> pre-stig-blueprint.toml <<EOF
+[[packages]]
+name = "scap-security-guide"
+version = "*"
+
+[[customizations.files]]
+path = "/etc/ostree/remotes.d/upstream.conf"
+data = """
+[remote "edge"]
+url=http://${HOSTIP}:${HOSTPORT}/repo/
+gpg-verify=false
+"""
+EOF
